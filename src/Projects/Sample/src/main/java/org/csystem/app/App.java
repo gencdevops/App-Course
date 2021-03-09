3c02777c21f6989c83c459e804dbd5a093688bb6
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-03-09 17:08:30 +0300

    tru

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -6,12 +6,7 @@ import java.util.Scanner;
 
 class App {
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        System.out.println("konsoldan bir sayı giriniz... : ");
-        int sayi = Integer.parseInt(kb.nextLine());
 
-        var a = sayi > 0 ? "ankara" : 50;
-        System.out.println(a);
 
 
 
