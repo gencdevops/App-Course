6ed01bb5d552ffd9ac6ee469efff1283247f6f4b
gencdevops <Kokluce112455>
2021-03-19 11:39:54 +0300

    Recursion.util factoriel

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -2,28 +2,13 @@
 package org.csystem.app;
 
 
-import java.sql.SQLOutput;
+import org.csystem.util.recursion.Util;
+
 
 class App {
     public static void main(String[] args) {
 
-      for(int i = 1; i <= 15; i++)
-          System.out.println(i +" Faktoriyel = " + Factoriel.faktoriyelHesapla(i));
-    }
-}
-
-class Factoriel {
-    public static long faktoriyelHesapla(int n) {
-        if(n <= 1) {
-            return 1;
-        }
-
-        long result = 1;
-
-        for(int i = 1; i <= n; i++)
-            result *= i;
-
-        return result;
-
+      for(int i = 1; i <= 13; i++)
+          System.out.println(i +" Faktoriyel = " + Util.factoriel(i));
     }
 }
