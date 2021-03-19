17b2fa73dbf47a38dc39ea7ad7e163e73110a4a9
gencdevops <Kokluce112455>
2021-03-19 12:42:00 +0300

    Util.java recursivwe algorithm examples

--- src/Projects/Sample/src/main/java/org/csystem/app/App.java
+++ src/Projects/Sample/src/main/java/org/csystem/app/App.java
@@ -8,7 +8,7 @@ import org.csystem.util.recursion.Util;
 class App {
     public static void main(String[] args) {
 
-      for(int i = 1; i <= 13; i++)
-          System.out.println(i +" Faktoriyel = " + Util.factoriel(i));
+
+
     }
 }
